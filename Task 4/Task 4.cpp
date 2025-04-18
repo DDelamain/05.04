6123d749bb916cd263b7050cc6519e7bbccc2d34
DDelamain <doroshenkodanila1@gmail.com>
2025-04-19 00:31:35 +0500

    Add files via upload

---
+++ Task 4/Task 4.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 0; i <= 9; i++) {
+        for (int j = 65; j <= 74; j++) {
+            char letter = j;
+            cout << letter << i << " ";
+        }
+        cout << "\n";
+    }
+}
